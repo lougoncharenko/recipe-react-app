3b1f0a4ea7a39d575b12e8297d2a59f5982322ba
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 18:40:03 -0500

    create use state for dynamically changing the page

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Recipe.css'
 
 const Recipe = () => {
+    const [recipe, setRecipe] = useState()
   return (
     <div>
-      <div className="recipe"></div>
+      <div className="recipe">
+        <h1></h1>
+        <img></img>
+        <div className='ingredients'></div>
+        <div className='instructions'></div>
+      </div>
       <div className="overlay"></div>
     </div>
   );

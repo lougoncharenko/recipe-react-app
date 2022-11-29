4c336fc3ed8c1521d73639701d5fe5a70da4e29a
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 18:28:58 -0500

    saving progress

---
+++ src/components/Recipe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recipe = () => {
+  return (
+    <div>
+      <div className="recipe"></div>
+      <div className="overlay"></div>
+    </div>
+  );
+}
+
+export default Recipe;

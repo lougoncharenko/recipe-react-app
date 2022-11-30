df2321d7fd04da18da89582e666e354601377fc6
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 19:04:18 -0500

    imported recipe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css'
+import Recipe from './components/Recipe';
 
 
 export default function App () {
     return(
-        <></>
+        <>
+        <Recipe />
+        </>
         );
 };

44ce346a169c3222f23bcf77d67a125036f3f089
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-11-29 19:04:48 -0500

    added fetch api function

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 import './Recipe.css'
 
-const Recipe = () => {
-    const [recipe, setRecipe] = useState()
+interface Recipe {
+    header: string,
+    image: string
+}
 
+const Recipe = () => {
+    const [recipe, setRecipe] = useState<Recipe>()
     const fetchRecipeApi = () => {
         fetch ("https://www.themealdb.com/api/json/v1/1/random.php") 
         .then(response =>{
@@ -15,19 +19,27 @@ const Recipe = () => {
         })
         .then(data =>{
             console.log(data)
+            const RecipeData: Recipe = {
+                header: data.meals[0].strMeal,
+                image: data.meals[0].strMealThumb
+            }
+            setRecipe(RecipeData)
         })
         .catch(error=> console.log('Fetch Error:', error));
     }
+    
+
+   const markup = recipe? (
+    <>
+       <h1>{recipe.header}</h1>
+        <img src={recipe.image} alt={recipe.header}></img> 
+    </>): <> <h1>Recipe Finder</h1> <img src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/table-filled-with-large-variety-of-food-shot-from-royalty-free-image-1659038707.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=640:*'></img> </>
+  
   return (
-    <div>
       <div className="recipe">
-        <h1></h1>
-        <img></img>
-        <div className='ingredients'></div>
-        <div className='instructions'></div>
+     {markup}
+     <button>Generate Recipe</button>
       </div>
-      <div className="overlay"></div>
-    </div>
   );
 }
 
